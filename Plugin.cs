78ec84782d28fd1aad5b7b11a395bce3e4351af3
madbrother <madbrother33@gmail.com>
2026-06-25 23:08:21 -0400

    min 0.1 max 10

--- Plugin.cs
+++ Plugin.cs
@@ -1,19 +1,10 @@
 ﻿using SPTarkov.DI.Annotations;
-using SPTarkov.Reflection.Patching;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Helpers;
-using SPTarkov.Server.Core.Models.Common;
-using SPTarkov.Server.Core.Models.Eft.Common;
-using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Enums;
 using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Models.Utils;
-using SPTarkov.Server.Core.Routers.ItemEvents;
 using SPTarkov.Server.Core.Servers;
-using SPTarkov.Server.Core.Services;
-using SPTarkov.Server.Core.Utils;
-using System.Globalization;
-using System.Reflection;
 
 namespace MadBro.QuestExpMultiplier
 {
@@ -23,7 +14,7 @@ namespace MadBro.QuestExpMultiplier
         public override string Name { get; init; } = "QuestExpMultiplier";
         public override string Author { get; init; } = "MadBrother";
         public override List<string>? Contributors { get; init; }
-        public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+        public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
         public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
         public override List<string>? Incompatibilities { get; init; }
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
@@ -38,11 +29,11 @@ namespace MadBro.QuestExpMultiplier
         public Task OnLoad()
         {
             float multiplier = 1;
+            string? message = null;
             try
             {
 
-                //multiplier = float.Parse(modUtils.ModConfig.Multiplier, CultureInfo.InvariantCulture);
-                multiplier = modUtils.ModConfig.Multiplier;
+                (multiplier, message) = NormalizeMultiplier(modUtils.ModConfig.Multiplier);
 
                 var quests = databaseServer.GetTables().Templates.Quests;
                 foreach ((_, var quest) in quests)
@@ -52,17 +43,36 @@ namespace MadBro.QuestExpMultiplier
                         var expReward = quest.Rewards.Values.SelectMany(rewardList => rewardList.Where(r => r.Type == RewardType.Experience));
                         foreach (var reward in expReward)
                         {
-                            reward.Value *= multiplier;
+                            if (reward.Value == null) continue;
+
+                            double newValue = reward.Value.Value * multiplier;
+                            reward.Value = (int)Math.Round((decimal)newValue);
                         }
                     }
                 }
             } catch (Exception ex)
             {
                 logger.Error(ex.Message);
+                return Task.CompletedTask;
             }
 
-            logger.Success($"QuestExpMultiplier harmony patch has successfully loaded! Multiplier applied: {multiplier}");
+            if (message != null)
+            {
+                logger.Warning("QuestExpMultiplier: " + message);
+                return Task.CompletedTask;
+            }
+
+            logger.Success($"QuestExpMultiplier has successfully loaded! Multiplier applied: {multiplier}");
             return Task.CompletedTask;
+        }
+
+        private static (float, string?) NormalizeMultiplier(float multiplier)
+        {
+            if (multiplier <= 0)
+                return (0.1f, "Multiplier cannot be less than or equal to 0. Setting to 0.1.");
+            if (multiplier > 10)
+                return (10f, "Multiplier cannot be greater than 10. Setting to 10.");
+            return (multiplier, null);
         }
     }
 }
